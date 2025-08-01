45a4a55cecd2a7571c6716f3ed16fb64648f1cd9
Sarah <s.tahar@xefi.fr>
2025-08-01 16:39:22 +0200

    clear dialog

--- EDP/EcoleDeLaPerformance/Models/SalesUp/Search.cs
+++ EDP/EcoleDeLaPerformance/Models/SalesUp/Search.cs
@@ -6,8 +6,8 @@ namespace EcoleDeLaPerformance.Ui.Models.SalesUp
 {
     public class Search
     {
-        [JsonPropertyName("selects")]
-        public List<Select> Selects { get; set; }
+        //[JsonPropertyName("selects")]
+        //public List<Select> Selects { get; set; }
 
         //[JsonPropertyName("filters")]
         //public List<Filter> Filters { get; set; }
